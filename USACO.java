8588f9971ca398e8091b61a279c73efcd209db09
jlin03 <jlin03@stuy.edu>
2019-03-06 14:44:51 -0500

    file reading code written

---
+++ USACO.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class USACO {
+
+   public static void bronze(String filename) {
+     int[][] pasture;
+     int e;
+     int n;
+     try {
+     File f = new File(filename);
+     Scanner scan = new Scanner(f);
+     String line = scan.nextLine();
+     String[] dataS = line.split(" ");
+     int[] data = new int[dataS.length];
+     for(int i = 0; i < dataS.length; i++) {
+       data[i] = Integer.parseInt(dataS[i]);
+     }
+     e = data[2];
+     n = data[3];
+     pasture = new int[data[0]][data[1]];
+     for(int r = 0; r < pasture.length; r++) {
+       line = scan.nextLine();
+       dataS = line.split(" ");
+       data = new int[dataS.length];
+       for(int i = 0; i < dataS.length; i++) {
+         data[i] = Integer.parseInt(dataS[i]);
+       }
+       for(int c = 0; c < pasture[r].length; c++) {
+         pasture[r][c] = data[c];
+       }
+     }
+     
+
+
+    }
+    catch(Exception x) {
+      System.out.println(x);
+    }
+
+   }
+
+   public static void main(String[] args) {
+     bronze(args[0]);
+
+   }
+
+
+
+
+
+}
